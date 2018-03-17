ac2af41539ce1a36bb124dfac699cf1b46f7bd5c
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-17 17:24:56 -0500

    Tricky

--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,32 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Dave Rapp
+  	Date: 3-17-18
 */
 
-// 1. Your predicted output 
+// 1. My predicted output was an error message!
  
 
 public class Tricky {
-
+	public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
 }
 
-// 3. Were you correct? Explain any differences
+// 3. I was incorrect because I thought that you would have to declare message input 
+// in the main and not in the statement below.
  
  
 
